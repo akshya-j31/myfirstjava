2053c618cfb4cfa25eb9386a264bc7c227c67fe0
Akshya <akshya.jain@afourtech.com>
2017-07-13 07:15:22 +0000

    This is commit for jenkins maven build Test.

--- NumberGenerator/src/main/java/com/mkyong/App.java
+++ NumberGenerator/src/main/java/com/mkyong/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This will work" );
+        System.out.println( "Hello World! This should work" );
     }
 }
